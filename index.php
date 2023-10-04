5adf8f98d58a66dfd37e8a80b57b326210934006
batruong1704 <batruong1704@gmail.com>
2023-10-04 22:15:02 +0700

    favicon.ico

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/Login.css">
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./public_html/favicon.ico" type="image/png">
     <title>Dangnhap</title>
 </head>
 
